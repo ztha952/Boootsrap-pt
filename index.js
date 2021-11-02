76b950981bf9e3aa3321d1706fb10403a524bd19
ZTHA <ztha2000@gmail.com>
2021-11-02 17:53:30 +0630

    first add

---
+++ index.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoib3NhbXV4IiwiYSI6ImNrdmdvdGUyaDR5YWIzMWxwZGxqanJvNWUifQ.9KNBKE3Yu_BZJpteCB67FA';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11',
+center: [ 96.1735, 16.8409],
+zoom: 14
+});
